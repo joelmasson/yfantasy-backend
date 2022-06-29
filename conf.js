f9972d784f7403db09662bbe273686d660dbd28a
joelmasson <joel@verve10.ca>
2022-06-29 09:24:12 -0400

    remove conf

--- conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'APP_KEY': 'dj0yJmk9Vzh4WnpCMW1hWU5YJmQ9WVdrOWNtNVJlSGRLWmtFbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTJm',
-  'APP_SECRET': 'ab962b0b2872a9dbadcfbbfe9a3a9cb60844b546',
-  'APP_URL': 'https://325b-2607-fea8-92e1-d600-9110-b418-5b8b-7e28.ngrok.io',
-  'SESSION_SECRET': 'Keyboard Cat',
-  'MONGODB': "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
-};
